6a4beb746cfcbc6aeaa2fb4e1bae0f1b53c8fc62
Ranik <er.rahul0314@gmail.com>
2024-05-24 14:58:40 +0530

    [Feature]: Implemented TodoItem component with edit and delete functionality

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,9 +1,37 @@
+import React, {useState} from "react";
+import './TodoItem.css';
 
-const TodoItem = () => {
+const TodoItem = ({todo, updateTodo, deleteTodo}) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [newTitle, setNewTitle] = useState('');
+
+    const handleUpdateTodo = () => {
+        updateTodo(todo.id,{...todo, title: newTitle});
+        setIsEditing(false);
+    }
     return(
-        <div>
-            <p>todoItem</p>
-        </div>
+        <li className="todo-item">
+            {isEditing ? (
+                <input
+                    type="text"
+                    value={newTitle}
+                    onChange={(e)=> setNewTitle(e.target.value)}>
+
+                </input>
+            ) : (
+            <span>{todo.title}</span>
+            )
+            }
+            <div className="buttons">
+                <button onClick={()=> deleteTodo(todo.id)}>Delete</button>
+                {isEditing ? (
+                    <button onClick={handleUpdateTodo}>Save</button>
+                ) : (
+                    <button onClick={()=> setIsEditing(true)}>Edit</button>
+                )
+            }
+            </div>
+        </li>
     )
 }
 
